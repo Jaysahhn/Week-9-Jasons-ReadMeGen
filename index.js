4d76746eebb074761315273354ceb7db8b1932a8
Jaysahhn <jasonmason96@yahoo.com>
2023-10-22 23:52:31 -0500

    moved some files around and deleted duplicates

---
+++ index.js
@@ -0,0 +1,30 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: "Cady POG",//questions[0],
+                name: 'project',
+            },])
+        .then((response) => writeToFile(response)
+        );
+
+
+
+}
+init();
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { }
+
+// TODO: Create a function to initialize app
+
+
+// Function call to initialize app
+
